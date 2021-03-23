3c305bd4970a9db42af7590fe25dfe348a169132
Rafael Rafael <rafael.arce@upr.edu>
2021-03-23 00:16:49 +0000

    Removed catch.

--- AStack.h
+++ AStack.h
@@ -1,5 +1,5 @@
 //
-// Created by Rafael Arce Nazario on 9/30/20.
+// Implementation of a stack using array
 //
 
 #ifndef ASTACK_H
@@ -11,17 +11,17 @@ using namespace std;
 
 class AStack : public Stack {
 private:
-    stackElementType *stackArray;    // Array holding stack
+    stackElementType *stackArray;         // Array holding stack
     static const int DEFAULT_SIZE = 10;
-    int maxSize;            // Maximum size of stack
-    int top;                // First free position at top
+    int maxSize;                          // Maximum size of stack
+    int top;                              // First free position at top
 public:
     // Constructors
     AStack(int size = DEFAULT_SIZE) : maxSize(size), top(0) {
         stackArray = new stackElementType[size]; // Create stackArray
     }
 
-
+    // Returns a representation of the stack, starting with the top element
     string toString() const {
         string outputString;
         for (int i = top-1; i >= 0; i--) {
@@ -51,7 +51,7 @@ public:
         return stackArray[--top];
     }
 
-
+    // Return the value of the top element
     stackElementType topValue() const{
         if (top == 0)
             throw std::out_of_range("trying to get topValue() from an empty AStack");
@@ -62,7 +62,7 @@ public:
 
     bool isEmpty() const { return top == 0; }
 };
-/* *** ODSAendTag: AStack2 *** */
+
 
 
 #endif //ASTACK_H
